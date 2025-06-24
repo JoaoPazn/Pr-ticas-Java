98330b5598bdfd94e4cc0ac7aa70aa229f118b76
Joao Nascimento <joaovitor223344joao@gmail.com>
2025-06-24 18:38:38 -0300

    Update Inpiração.java

--- Inpiração.java
+++ Inpiração.java
@@ -9,14 +9,14 @@ public class Main {
         float[] produtos_precos = new float[4];
         String[] produtos_nomes = new String[4];
 
-        for (int i = 0; i <= produtos_nomes.length; i++) {
+        for (int i = 0; i < produtos_nomes.length; i++) {
             System.out.println("Digite o nome do prod" + (i + 1));
             produtos_nomes[i] = input.next();
             System.out.println("Digite o preço do produto" + (i + 1));
             produtos_precos[i] = input.nextFloat();
         }
 
-        for (int i = 0; i <= produtos_nomes.length; i++) {
+        for (int i = 0; i < produtos_nomes.length; i++) {
             System.out.printf("%s - R$ %.2f%n", produtos_nomes[i], produtos_precos[i]);
         }
         
